e5dcd1e7679dfc697b85747c874911b21a6c0051
Dom Cabral <domcabral@protonmail.com>
2022-07-25 11:30:26 -0400

    Adicionando SHEBANG

---
+++ install_zabbix6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# install_zabbix6.sh - Sript instalação e configuração do Zabbix 6 stable (20.04 LTS ou superior)
+#
+# Website:       https://www.linkedin.com/in/domcabral/
+# Autor:         André Cabral (Dom Cabral)  
+#
+# ------------------------------------------------------------------------ #
+#
+# COMO USAR?
+#   $ ./install_zabbix6.sh
+#
+# ----------------------------- VARIÁVEIS ----------------------------- #
